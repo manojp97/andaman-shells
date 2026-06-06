24f4f8f948ad8fae6f5d003f09cb8ae505d4916b
manojp97 <manojprajapati97563684@gmail.com>
2026-06-06 16:24:56 +0530

    frontend is updated

--- src/components/PackageCards.jsx
+++ src/components/PackageCards.jsx
@@ -4,20 +4,34 @@ import { Button } from "./ui/button";
 const PackageCards = ({ image, title }) => {
   return (
     <div>
+      <div className="overflow-hidden  shadow-lg hover:shadow-xl transition-all duration-300">
+        <img src={image} alt={title} className="w-full h-full object-cover" />
+      </div>
 
-    <div className="overflow-hidden  shadow-lg hover:shadow-xl transition-all duration-300">
-      <img src={image} alt={title} className="w-full h-full object-cover" />
+      <div className="flex items-center justify-center gap-3 mt-4 flex-wrap">
+        <a
+          href="https://wa.me/919599227385"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="rounded border bg-green-500 hover:bg-green-600 text-white p-5">
+            WhatsApp
+          </Button>
+        </a>
+
+        <a href="tel:+919599227385">
+          <Button className="rounded border bg-sky-400 hover:bg-sky-500 text-white p-5">
+            Get Callback
+          </Button>
+        </a>
+
+        <a href="#contact">
+          <Button className="rounded border bg-red-600 hover:bg-red-700 text-white p-5">
+            Enquire
+          </Button>
+        </a>
+      </div>
     </div>
-
-    <div className="flex items-center justify-center gap-3 mt-4">
-        <Button className="rounded border bg-green-500 hover:bg-green-600 text-white p-5">Whatsapp</Button>
-        <Button className="rounded border bg-sky-400 hover:bg-sky-500 text-white p-5">Get Callback</Button>
-        <Button className="rounded border bg-red-600 hover:bg-red-700 text-white p-5">Enquire</Button>
-    </div>
-
-    </div>
-   
-
   );
 };
 
